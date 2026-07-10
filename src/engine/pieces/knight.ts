df15aa877f1ec75e2608c28ced16d8ae7f02bb47
Ioana Maraloi <ioana.maraloi@softwire.com>
2026-07-10 14:38:48 +0300

    fixed sixth test. knight

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,23 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let result : Array<Square> = [];
+        let position = board.findPiece(this);
+        let move : Array<number> = [-2, -1, 1, 2];
+        let potentialRow = position.row;
+        let potentialCol = position.col;
+        for (let i = 0; i < move.length; i++) {
+            for(let j = 0; j < move.length; j++) {
+                if (Math.abs(move[i]) == Math.abs(move[j])){
+                    continue;
+                }
+                 potentialRow = position.row + move[i];
+                 potentialCol = position.col + move[j];
+                 if (potentialRow >= 0 && potentialCol >= 0 && potentialRow < gameSettings.BOARD_SIZE && potentialCol < gameSettings.BOARD_SIZE) {
+                     result.push(new Square(potentialRow, potentialCol));
+                 }
+            }
+        }
+        return result;
     }
 }
--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -2,9 +2,6 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from "../square";
-import gameSettings from "../gameSettings";
-import Bishop from "./bishop";
-import player from "../player";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
