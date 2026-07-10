d85d103e4cf9c30a4374d394ae222b1963118467
Ioana Maraloi <ioana.maraloi@softwire.com>
2026-07-10 12:36:40 +0300

    fixed first test. pawn

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import player from "../player";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,15 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+       let position : Square = board.findPiece(this);
+       let result:Array<Square> = [];
+       if (this.player == player.WHITE ) {
+           result.push(new Square(position.row + 1, position.col));
+           // result.push(new Square(position.row + 2, position.col));
+       }else{
+           result.push(new Square(position.row - 1, position.col));
+           // result.push(new Square(position.row + 2, position.col));
+       }
+        return result;
     }
 }
