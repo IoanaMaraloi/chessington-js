2ed76b97cacea91d561f8ba7d66192b641c64cb6
Ioana Maraloi <ioana.maraloi@softwire.com>
2026-07-10 13:57:51 +0300

    fixed third test. rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,24 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        // return new Array(0);
+        let result : Square[] = [];
+        let position : Square = board.findPiece(this);
+        let row: number = position.row;
+        let col: number = position.col;
+        for(let i = 0; i < gameSettings.BOARD_SIZE; i++) {
+            if(i == row){
+                continue;
+            }
+            result.push(new Square(i, position.col));
+        }
+        for(let i = 0; i < gameSettings.BOARD_SIZE; i++) {
+            if(i == col){
+                continue;
+            }
+            result.push(new Square(position.row, i));
+        }
+
+        return result;
     }
 }
